a921fc05964abd0530c8eb72385d28e24b53346d
sinchana.p.swarna@gmail.com <83894491+sinchana-P@users.noreply.github.com>
2022-03-21 23:13:24 -0700

    first commit uploading code file

---
+++ calculator.js
@@ -0,0 +1,37 @@
+//program for a simple calculator
+const prompt = require('prompt-sync')();
+
+let result;
+const operator = prompt('enter operator ( either = +,-,*,/ or % ) : ');
+
+const num1 = parseFloat(prompt('enter first number : '));
+const num2 = parseFloat(prompt('enter second number : '));
+
+switch(operator) {
+    case '+' : result = num1 + num2 ;
+               console.log(`${num1} + ${num2} = ${result}`);
+               break;
+    
+    case '-' : result = num1 - num2 ;
+               console.log(`${num1} - ${num2} = ${result}`);
+               break;
+               
+    case '*' : result = num1 * num2 ; 
+               console.log(`${num1} * ${num2} = ${result}`);
+               break;
+
+    case '/' : result = num1 / num2 ;
+               console.log(`${num1} / ${num2} = ${result}`);
+               break;
+
+    case '%' : result = num1 % num2 ;
+               console.log(`${num1} % ${num2} = ${result}`);
+               break;
+       
+    default : console.log(`Invalid operator`);
+              break;             
+
+}
+
+
+
